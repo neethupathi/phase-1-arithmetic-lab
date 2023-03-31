7ab4492f496e821797a30c435b93c1e854dcbd7d
neethupath <pathnee222gmail.com>
2023-03-30 20:11:23 -0400

    math

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let multiply;
+
+    let num1=2;
+    let num2=31;
+     multiply=num1*num2;
+
+
+    let random;
+    random=Math.floor(Math.random()*10 +1);
+
+    let mod;
+    let num3=4;
+let num4=1;
+mod=num3/num4;
+
+let max=20;
+max=Math.max(1,2,3,20);
